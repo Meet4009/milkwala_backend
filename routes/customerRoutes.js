f707d910049d7c8052a58106a77ea6c47f0cb57f
Meet4009 <meetsolanki4009@gmail.com>
2025-02-22 11:45:14 +0530

    add customer Routes

--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -6,9 +6,13 @@ const customerControl = require('../controllers/customerController');
 
 const { authentication, authorize } = require('../middlewares/auth');
 
+// Admin routes
 route.post('/register', customerControl.register);
 route.post('/login', customerControl.login);
 route.get('/logout', customerControl.logout);
 route.get('/', authentication, authorize, customerControl.getAllCustomers);
+route.get('/:id', authentication, authorize, customerControl.getsingleCustomer);
+route.put('/update/:id', authentication, authorize, customerControl.updateCustomer);
+route.delete('/delete/:id', authentication, authorize, customerControl.deleteCustomer);
 
 module.exports = route;
